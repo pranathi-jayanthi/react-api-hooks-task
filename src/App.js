aed3a5a8f05879599ba01aa8a390ffc21771e038
Pranathi-Jayanthi <113671307+Pranathi-Jayanthi@users.noreply.github.com>
2023-08-27 22:27:45 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+const [resourceType, setResourceType] = useState('posts');
+const [items, setItems] = useState([]);
+
+useEffect(() => {
+  fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    .then(response => response.json())
+    .then(json => setItems(json))
+    .catch(err => console.log('Error fetching data'))
+}, [resourceType]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <button onClick={() => setResourceType('posts')}>Posts</button>
+            <button onClick={() => setResourceType('users')}>Users</button>
+            <button onClick={() => setResourceType('comments')}>Comments</button>
+        </div>
+        <h1>{resourceType}</h1>
+        {items.map(item => {
+          return <pre>{JSON.stringify(item)}</pre>
+        })}
     </div>
   );
 }
